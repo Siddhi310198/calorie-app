67da2a2756703a592cf8df8f58847f2190f1aabd
Siddhi310198 <siddhithakur31@yahoo.com>
2026-04-28 16:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,8 @@ if uploaded_file:
                             {"type": "text", "text": prompt},
                             {
                                 "type": "image_url",
-                                "image_url": f"data:image/png;base64,{base64_image}"
+                                "image_url": {"url": f"data:image/png;base64,{base64_image}"
+                                             }
                             }
                         ]
                     }
